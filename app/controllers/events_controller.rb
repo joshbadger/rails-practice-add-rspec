78c4505cbb0cf13cdd75b62451112c319c785f61
joshBadger <badger.joshua@gmail.com>
2014-11-04 13:45:20 -0700

    restricted some gems to development and test

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -44,7 +44,7 @@ class EventsController < ApplicationController
 
   def destroy
     @event = Event.find(params[:id])
-    @event.destroy
+    @event.destroy(event_params)
     redirect_to events_path
   end
 
